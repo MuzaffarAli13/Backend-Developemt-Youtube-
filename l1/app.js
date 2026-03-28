1cb234868e1d3aa52d62af5c5b6d1b884b1cfc9e
Muzaffar Ali <kalwar12khan@gmail.com>
2026-03-28 23:35:03 +0500

    first commit

---
+++ l1/app.js
@@ -0,0 +1,128 @@
+// console.log("Hello World");
+// console.log("Hello World");
+
+// DataTypes 
+// String - "Muzaffar"
+// Number -->  123
+// boolean ->  true/false 
+// obect --> {}
+// symbols - > @#$%^&
+// array - > []
+
+// variable
+// var name = "Ali";
+// console.log(name);
+
+
+// var name = "muzaffar"
+// console.log(name);
+
+
+// let age = 20;
+// console.log(age);
+
+// age = 19;
+// console.log(age);
+
+
+// const city = "Karachi";
+// console.log(city);
+
+
+
+// Operators
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(8/2);
+// console.log(8/2);
+// console.log(2**2);
+
+// let a = 20;
+// let b = 10;
+// let total = a + b;
+// console.log(total);
+
+
+
+// Conditions
+
+// let name = "muzafar";
+
+// if(name === "muzafr"){
+//    console.log(name);
+// }else{
+//     console.log("try again");
+// }
+
+// let name = "hadi";
+// let password = "123";
+
+// if(name === "muzafar" && password === "123"){
+//     console.log("loggin");
+// }else{
+//     console.log("Try Again!");
+// }
+
+// let score = 10;
+
+// if(score >= 50){
+//     console.log("50");
+// }else if(score >= 10){
+//     console.log(10);
+// }else{
+//     console.log("Not Found");
+// }
+
+// for(let i=0; i <= 10; i++){
+//   console.log("hello Word");
+// }
+
+// let name = "muzaffar"
+// while (name === "muzaffar") {
+//     console.log("good");
+   
+// }
+
+// let count = 0;
+// while(count <= 10){
+//     console.log(count);
+//     count++
+// }
+
+
+
+
+// // Functions
+// function greet(){
+//     return "hello"
+// }
+
+// let result = greet()
+// console.log(result);
+
+    //        parameter
+// function greet(name){
+//    return `Salam ${name}`
+// }
+
+// let result = greet("Muzafar ALi") // attribute
+// let res = greet("Ali") // attribute
+// console.log(result);
+// console.log(res);
+
+// Sum
+// function sum(a,b){
+//     return a + b
+// };
+
+// console.log(sum(12,10));
+
+// Arrow Function
+let greet = (name,age)=>{
+    return `Hello ${name} ${age}`
+}
+
+let res = greet("khan",19);
+console.log(res);
